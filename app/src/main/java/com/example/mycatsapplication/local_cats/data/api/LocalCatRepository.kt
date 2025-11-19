19620fc797252da5f4fe2dee0d0da4439debae67
dencharski <dencharski@yandex.ru>
2025-11-19 18:57:47 +0200

    -add SharedPreferences in LocalCatRepositoryImpl.kt

--- app/src/main/java/com/example/mycatsapplication/local_cats/data/api/LocalCatRepository.kt
+++ app/src/main/java/com/example/mycatsapplication/local_cats/data/api/LocalCatRepository.kt
@@ -4,7 +4,7 @@ import com.example.mycatsapplication.walking_cats.domain.models.CatDataModel
 
 interface LocalCatRepository {
     suspend fun getAllLocalCats():List<CatDataModel>
-    suspend fun addCat(cat: CatDataModel)
-    suspend fun deleteCat(cat: CatDataModel)
+    suspend fun addCat(cat: CatDataModel): List<CatDataModel>
+    suspend fun deleteCat(cat: CatDataModel): List<CatDataModel>
     suspend fun getCatsIds(): List<String>
 }
